931daa55c3d62810761b422ea435dd4120864d5b
jianggoudan <new5spring@126.com>
2020-08-17 03:19:14 +1200

    ASD

--- hello_2 (1)/src/view/Iview_router_1/Iview_router_1_r2.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        'contentBar2'组件，对应的vue文件名：Iview_router_1_r2
-    </div>
-</template>
--- hello_2 (1)/src/view/Iview_router_1/Iview_router_1_r3.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        'contentBar3'组件，对应的vue文件名：Iview_router_1_r3
-    </div>
-</template>
